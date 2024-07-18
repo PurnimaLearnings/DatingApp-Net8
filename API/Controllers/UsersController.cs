9a7e9619a7e46ccd26c5ac2442b6d5900bd8d030
PurnimaLearnings <Purnima.rai5@gmail.com>
2024-07-18 09:08:04 +0530

    First commit Eos 2

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using API.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API;
+
+[ApiController]
+[Route("api/[Controller]")]
+public class UsersController(DataContext context): ControllerBase
+{
+    [HttpGet]
+    public ActionResult<IEnumerable<AppUser>> GetUsers(){
+        var users= context.Users.ToList();
+        return Ok(users);
+    }
+[HttpGet("{id}")]//api/users/2
+    public ActionResult<IEnumerable<AppUser>> GetUser(int id){
+        var user= context.Users.Find(id);
+        if(user==null)
+        {
+            return NotFound();
+        }
+
+
+        return Ok(user);
+    }
+}
