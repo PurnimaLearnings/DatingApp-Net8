56b22b1db8432abaa01ba2a9341f06310d8d5273
PurnimaLearnings <Purnima.rai5@gmail.com>
2024-09-20 14:12:29 +0530

    Added reusable datepicker component.

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,35 +1,72 @@
-import { Component,  EventEmitter,  inject,  input, Output, output } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { Component,  EventEmitter,  inject,  input, OnInit, Output, output } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
 import { AccountsService } from '../_services/accounts.service';
 import { ToastrService } from 'ngx-toastr';
+import { JsonPipe, NgIf } from '@angular/common';
+import { TextInputComponent } from "../_forms/text-input/text-input.component";
+import { createInjectableType } from '@angular/compiler';
+import { DatePickerComponent } from "../_forms/date-picker/date-picker.component";
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule, JsonPipe, NgIf, TextInputComponent, DatePickerComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  ngOnInit(): void {
+    this.initializeForm();
+this.maxDate.setFullYear(this.maxDate.getFullYear()-18);
+  }
  private accountService=inject(AccountsService);
  toastr=inject(ToastrService);
+ private fb=inject(FormBuilder);
   // @Input() usersFromHomeComponent:any;
  // @Output() cancelRegister =new EventEmitter();
   cancelRegister=output<boolean>();
   usersFromHomeComponent=input.required<any>();
 
 
-  model:any={}
+model:any={}
+registerForm:FormGroup=new FormGroup({});
+maxDate = new Date();
+
+initializeForm(){
+  this.registerForm=this.fb.group({
+    gender:['male'],
+    username: ['', Validators.required],
+    knownAs:['',Validators.required],
+    dateOfBirth:['',Validators.required],
+    city:['',Validators.required],
+    country:['',Validators.required],
+    password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+    confirmPassword: ['', [Validators.required, this.matchValues('password')]],
+  });
+
+  this.registerForm.controls['password'].valueChanges.subscribe({
+    next: ()=>this.registerForm.controls['confirmPassword'].updateValueAndValidity()
+  });
+}
+
+matchValues(matchTo:string):ValidatorFn{
+return (control:AbstractControl)=>{
+  return control.value===control.parent?.get(matchTo)?.value?null:{isMatching:true}
+}
+}
+
 register(){
-  this.accountService.register(this.model).subscribe({
-    next: response=>{
-      console.log(response);
-      this.cancel();
-    },
-    error:error=>this.toastr.error(error.error)
+ console.log(this.registerForm.value);
+ 
+    // this.accountService.register(this.model).subscribe({
+  //   next: response=>{
+  //     console.log(response);
+  //     this.cancel();
+  //   },
+  //   error:error=>this.toastr.error(error.error)
     
-  })
-  console.log(this.model);
+  // })
+  // console.log(this.model);
 
 }
 cancel(){
